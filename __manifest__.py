1aea7b7d02e56e77908385bb134a78ce7af571a2
Collaborapro <155647018+Collaborapro@users.noreply.github.com>
2024-11-20 12:54:13 +0100

    aggiornamento manyfest

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,23 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Custom Product Labels for Receipts',
-    'version': '1.0',
-    'category': 'Warehouse',
-    'summary': 'Custom labels for products in receipts',
-    'description': 'Generate product labels with receipt details.',
-    'depends': ['stock', 'product'],
-    'data': [
-        'report/custom_product_label_template.xml',
-        'report/custom_product_label_report.xml',
+    "name": "Custom Product Labels for Receipts",
+    "version": "1.0",
+    "category": "Warehouse",
+     "author": "Necte Eloisa Montevecchi",
+    "version": "16.0.0.1",
+    "website": "https://www.necte.it",
+    "summary": "Custom labels for products in receipts",
+    "description": "Generate product labels with receipt details.",
+    "depends": ["stock", "product"],
+    "data": [
+        "report/custom_product_label_template.xml",
+        "report/custom_product_label_report.xml",
     ],
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
+
+
+
+
+
