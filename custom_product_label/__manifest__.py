9716939765bb2c3f942615faf7abfeb4a5c62a2d
ncetrulo <ncetrulo@2esseti.it>
2024-11-27 10:16:39 +0100

    [UPD] custom_product_label

--- custom_product_label/__manifest__.py
+++ custom_product_label/__manifest__.py
@@ -4,7 +4,7 @@
     "version": "1.0",
     "category": "Warehouse",
      "author": "Necte Eloisa Montevecchi",
-    "version": "16.0.0.2",
+    "version": "16.0.0.3",
     "website": "https://www.necte.it",
     "summary": "Custom labels for products in receipts",
     "description": "Generate product labels with receipt details.",
