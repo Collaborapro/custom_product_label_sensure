1e6975b4efc1aeb7c655444a2375393b0168b96b
ncetrulo <ncetrulo@2esseti.it>
2025-01-10 11:03:15 +0100

    [UPD] custom_product_label

--- custom_product_label/wizards/product_label_layout.py
+++ custom_product_label/wizards/product_label_layout.py
@@ -1,10 +1,39 @@
 from odoo import models
+from odoo.exceptions import UserError
 
 
 class ProductLabelLayout(models.TransientModel):
     _inherit = "product.label.layout"
 
     def _prepare_report_data(self):
+        active_model = self.env.context.get("active_model", "stock.picking")
+
+        if active_model in ("stock.lot", "stock.move.line", "stock.quant"):
+            active_ids = self.env.context.get("active_ids", [])
+            record_ids = self.env[active_model].browse(active_ids)
+
+            if self.custom_quantity <= 0:
+                raise UserError("Devi inserire una quantità positiva.")
+
+            if self.print_format == "dymo":
+                xml_id = "product.report_product_template_label_dymo"
+
+            elif "x" in self.print_format:
+                xml_id = "product.report_product_template_label"
+
+            else:
+                xml_id = ""
+
+            product_ids = record_ids.filtered("product_id").mapped("product_id")
+            data = {
+                "active_model": "product.product",
+                "quantity_by_product": {p.id: self.custom_quantity for p in product_ids},
+                "layout_wizard": self.id,
+                "price_included": "xprice" in self.print_format
+            }
+
+            return xml_id, data
+
         xml_id, data = super()._prepare_report_data()
         picking_id = self.move_line_ids and self.move_line_ids[0].picking_id or False
 
