4327e5b75c505b656b059a52a0319415991af674
ncetrulo <ncetrulo@2esseti.it>
2025-01-10 10:54:35 +0100

    [UPD] custom_product_label

--- custom_product_label/wizards/lot_label_layout.py
+++ custom_product_label/wizards/lot_label_layout.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from odoo import models
 
 
@@ -9,12 +11,35 @@ class LotLabelLayout(models.TransientModel):
 
         active_model = self.env.context.get("active_model", "stock.picking")
 
-        if active_model in ("stock.lot", "stock.move.line", "stock.quant") and self.label_quantity == "lots":
+        if active_model in ("stock.lot", "stock.move.line", "stock.quant"):
             active_ids = self.env.context.get("active_ids", [])
-            docids = (
-                active_model == "stock.lot" and self.ids
-                or self.env[active_model].browse(active_ids).mapped("lot_id").ids
-            )
+            record_ids = self.env[active_model].browse(active_ids)
+            lot_ids = active_model == "stock.lot" and self or record_ids.mapped("lot_id")
+
+            if self.label_quantity == "lots":
+                docids = lot_ids.ids
+
+            else:
+                quantity_by_lot = defaultdict(int)
+
+                for record in record_ids:
+                    if active_model == "stock.lot":
+                        quantity_by_lot[record.id] += 1
+
+                    else:
+                        if record.product_uom_id.category_id.id == self.env.ref("uom.product_uom_categ_unit", False).id:
+                            qty = active_model == "stock.quant" and record.inventory_quantity or record.qty_done
+                            quantity_by_lot[record.lot_id.id] += int(qty)
+
+                        else:
+                            quantity_by_lot[record.lot_id.id] += 1
+
+
+                docids = []
+
+                for lot_id, qty in quantity_by_lot.items():
+                    docids.extend([lot_id] * qty)
+
             xml_id = self.print_format == "zpl" and "stock.label_lot_template" or "stock.action_report_lot_label"
             report_action = self.env.ref(xml_id).report_action(docids)
             report_action.update({"close_on_report_download": True})
